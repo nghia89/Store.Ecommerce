3b2964dc1492d0e004956aca5c94da33ca8b2b0e
Nguyen Thanh Nghia <37038823+nghia89@users.noreply.github.com>
2023-05-24 23:07:43 +0700

    fluent product

---
+++ aspnet-core/src/Store.Ecommerce.EntityFrameworkCore/Configurations/Products/ProductConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Store.Ecommerce.Catalog.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Ecommerce.Configurations.Products
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable(EcommerceConsts.DbTablePrefix + "Products");
+            builder.HasKey(x => x.Id);
+            builder.Property(x=>x.Name).HasMaxLength(250).IsRequired() ;
+            builder.HasIndex(x => x.SKU);
+            builder.Property(x => x.Slug)
+              .HasMaxLength(250).IsRequired(); 
+            builder.Property(x => x.ThumbnailPicture)
+           .HasMaxLength(250);
+            builder.Property(x => x.ShortDescription)
+             .HasMaxLength(250);
+            builder.Property(x => x.MetaDescription)
+             .HasMaxLength(250);
+            builder.Property(x => x.MetaKeywords)
+            .HasMaxLength(250);
+            builder.Property(x=>x.SKU).HasMaxLength(50).IsUnicode(false);
+        }
+    }
+}
